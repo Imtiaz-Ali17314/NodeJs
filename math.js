21a791cfca253e762fc6fb9a6d6471bf38e2d01f
Imtiaz Ali <imtiazali17314@gmail.com>
2025-03-25 14:41:10 +0500

    module.exports  and require property for excess data of one file to another

---
+++ math.js
@@ -0,0 +1,11 @@
+const sum = (a, b) => a + b;
+const sub = (a, b) => a - b;
+const mul = (a, b) => a * b;
+PI = 3.14;
+
+module.exports = {
+  sum: sum,
+  sub: sub,
+  mul: mul,
+  PI: PI,
+};
---
+++ script.js
@@ -0,0 +1,6 @@
+const math = require("./math.js");
+
+console.log(math.sum(5, 6));
+console.log(math.mul(5, 6));
+
+console.log(math.PI);
