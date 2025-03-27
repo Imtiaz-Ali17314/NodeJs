87499799de1bf9be3ebbb406d0fea43a09364799
Imtiaz Ali <imtiazali17314@gmail.com>
2025-03-27 15:56:23 +0500

    routing query strings and send response

--- Express/index.js
+++ Express/index.js
@@ -1,13 +1,29 @@
+// Connect with server
+
 const express = require("express");
 const app = express();
 
 const port = 8080;
 
+// Server is listening for request
+
 app.listen(port, () => {
   console.log(`server is listening at ${port}`);
 });
 
+// Request Parse and routing and also send response
+
 app.get("/:username/:password", (req, res) => {
   let { username, password } = req.params;
-  res.send(`Welcome to the page of @${username}`);
+  res.send(`<h1>Welcome to the page of @${username}</h1>`);
+});
+
+// Request routing with query strings
+
+app.get("/search", (req, res) => {
+  let { q } = req.query;
+  if (!q) {
+    res.send("nothing to search");
+  }
+  res.send(`<h1>Search results for query: ${q}</h1>`);
 });
