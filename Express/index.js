e2fcf531012646a63d38029c306127358f50f85a
Imtiaz Ali <imtiazali17314@gmail.com>
2025-03-27 15:11:42 +0500

    get method is for send reponse to a specific path.Post method is also for this porpose

--- Express/index.js
+++ Express/index.js
@@ -7,7 +7,27 @@ app.listen(port, () => {
   console.log(`server is listening at ${port}`);
 });
 
-app.use((req, res) => {
-  console.log("request received");
-  res.send("This is a basic response");
+// app.use((req, res) => {
+//   console.log("request received");
+//   res.send("This is a basic response");
+// });
+
+app.get("/", (req, res) => {
+  res.send("You contract with root path");
+});
+
+app.get("/search", (req, res) => {
+  res.send("You contract with search path");
+});
+
+app.get("/help", (req, res) => {
+  res.send("You contract with help path");
+});
+
+app.get("*", (req, res) => {
+  res.send("this path does not exist");
+});
+
+app.post("/", (req, res) => {
+  res.send("You send a post request");
 });
