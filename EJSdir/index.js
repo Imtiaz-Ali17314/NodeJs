277fb6a1bb8dcb426934c0df0ce8f0f868588bc5
Imtiaz Ali <imtiazali17314@gmail.com>
2025-03-29 14:53:47 +0500

    get data from database and pass it to ejs

--- EJSdir/index.js
+++ EJSdir/index.js
@@ -1,20 +1,32 @@
+// Require Express
 const express = require("express");
 const app = express();
 const path = require("path");
 
+// select Port
 const port = 8080;
 
+// Set Tampelet engine as EJS
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "/views"));
 
+// Render function send a tampelet in the form of ejs file as response
 app.get("/", (req, res) => {
   res.render("home.ejs");
 });
 
+// Get data from server and pass it to ejs
+app.get("/rolldice", (req, res) => {
+  let diceVal = Math.floor(Math.random() * 6) + 1;
+  res.render("rolldice.ejs", { diceVal });
+});
+
+// Send function sends a general response
 app.get("/hello", (req, res) => {
   res.send("hello");
 });
 
+// Server is listening for requests
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
