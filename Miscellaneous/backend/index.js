bb0a971c0cde90b1947cd2df6f5e60384d39feec
Imtiaz Ali <imtiazali17314@gmail.com>
2025-03-30 17:13:44 +0500

    encoded and json data parse into express object

--- Miscellaneous/backend/index.js
+++ Miscellaneous/backend/index.js
@@ -2,13 +2,18 @@ const express = require("express");
 const app = express();
 const port = 8080;
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 app.get("/register", (req, res) => {
   let { username, password } = req.query;
   res.send(`Standered GET request from ${username}`);
 });
 
 app.post("/register", (req, res) => {
-  res.send("Standered POST request");
+  console.log(req.body);
+  let { username, password } = req.body;
+  res.send(`Standered POST request from ${username}`);
 });
 
 app.listen(port, () => {
