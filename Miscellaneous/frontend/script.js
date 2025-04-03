20775b6b656bf3737b78fac31e866c2f102bb10e
Imtiaz Ali <imtiazali17314@gmail.com>
2025-04-03 14:50:43 +0500

    Prototype

--- Miscellaneous/frontend/script.js
+++ Miscellaneous/frontend/script.js
@@ -1,3 +1,16 @@
 arr = [1, 2, 3, 4, 5];
 
 arr.push(6, 7, 9);
+
+arr.sayhello = () => {
+  console.log("Hi! I am array");
+};
+
+arr2 = [12, 34, 5, 7, 98];
+
+arr2.sayhello = () => {
+  console.log("Hi! I am array");
+};
+
+console.log("arr prototype: ", arr.__proto__);
+console.log("Array prototype: ", Array.prototype);
