0e7e25dad6e23245a77cf4fb6889640772ecacc2
Imtiaz Ali <imtiazali17314@gmail.com>
2025-04-03 15:40:06 +0500

    Inheritance in Classes. How to inherit child from parent class...  Use of extends and super keywords

--- Miscellaneous/frontend/script.js
+++ Miscellaneous/frontend/script.js
@@ -1,14 +1,31 @@
-// Classes in JS
+// Inheritance in Classes
 
+// Parent class ---- Base class
 class Person {
   constructor(name, age) {
     this.name = name;
     this.age = age;
   }
   talk() {
-    console.log(`Hello! I am ${this.name}`);
+    console.log(`Hi! I am ${this.name}`);
   }
 }
 
-let p1 = new Person("Ali", 12);
-let p2 = new Person("hSSAN", 34);
+// Child class
+class Student extends Person {
+  constructor(name, age, marks) {
+    super(name, age);
+    this.marks = marks;
+  }
+}
+
+// Child class
+class Teacher extends Person {
+  constructor(name, age, subject) {
+    super(name, age);
+    this.subject = subject;
+  }
+}
+
+let stu1 = new Student("Rasib", 11, 90);
+let teach1 = new Teacher("Kamil", 43, "Urdu");
