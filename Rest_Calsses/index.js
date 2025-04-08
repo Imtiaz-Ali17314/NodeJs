8dbabe3e1cde028443eb015d2e490ee51a04a2d0
Imtiaz Ali <imtiazali17314@gmail.com>
2025-04-08 15:51:56 +0500

    Install method override package and convert post request into patch request. create new edit page on which post can edit and update it

--- Rest_Calsses/index.js
+++ Rest_Calsses/index.js
@@ -3,9 +3,11 @@ const app = express();
 const port = 8080;
 const path = require("path");
 const { v4: uuidv4 } = require("uuid");
+const methodOverride = require("method-override");
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(methodOverride("_method"));
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
@@ -30,10 +32,12 @@ let posts = [
   },
 ];
 
+//  Read Operation
 app.get("/posts", (req, res) => {
   res.render("index.ejs", { posts });
 });
 
+// Create Operation
 app.get("/posts/new", (req, res) => {
   res.render("new.ejs");
 });
@@ -45,10 +49,25 @@ app.post("/posts", (req, res) => {
   res.redirect("/posts"); // Redirect Page
 });
 
+//  Update Operation
 app.get("/posts/:id", (req, res) => {
   let { id } = req.params;
   let post = posts.find((p) => id === p.id);
   res.render("show.ejs", { post }); // Render Page
+});
+
+app.patch("/posts/:id", (req, res) => {
+  let { id } = req.params;
+  let newContent = req.body.content;
+  let post = posts.find((p) => id === p.id);
+  post.content = newContent;
+  res.redirect("/posts");
+});
+
+app.get("/posts/:id/edit", (req, res) => {
+  let { id } = req.params;
+  let post = posts.find((p) => id === p.id);
+  res.render("edit.ejs", { post });
 });
 
 app.listen(port, () => {
