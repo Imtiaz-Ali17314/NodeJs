b569ba44ff43ec201fed621c35484f9fc8d9e485
Imtiaz Ali <imtiazali17314@gmail.com>
2025-04-08 16:08:47 +0500

    Delete operation perform byh using delete method

--- Rest_Calsses/index.js
+++ Rest_Calsses/index.js
@@ -70,6 +70,13 @@ app.get("/posts/:id/edit", (req, res) => {
   res.render("edit.ejs", { post });
 });
 
+// Delete Operation
+app.delete("/posts/:id", (req, res) => {
+  let { id } = req.params;
+  posts = posts.filter((p) => id !== p.id);
+  res.redirect("/posts");
+});
+
 app.listen(port, () => {
   console.log("Listening to port : 8080");
 });
